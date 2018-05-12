34ac44733d344e8a8ed2779fdaf13b57973f1667
Eric Laursen <ericjlaursen@gmail.com>
2018-05-12 17:43:53 -0400

    Includes helpful comment.

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mongojs = require('mongojs');
 const db = mongojs('mongodb://jakeLaursen:Jake8Snake6@ds133964.mlab.com:33964/mytasklist', ['tasks'])
 
-//GET
+//GET All tasks
 router.get('/tasks', (req,res,next) => {
 	db.tasks.find((err, tasks) => {
 		if(err){
