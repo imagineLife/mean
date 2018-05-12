72ac89bc70a5814a4b7d20eaf89a5b1196c8d1fa
Eric Laursen <ericjlaursen@gmail.com>
2018-05-12 17:41:34 -0400

    Includes mongo requires.

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongojs = require('mongojs');
+const db = mongojs('mongodb://jakeLaursen:Jake8Snake6@ds133964.mlab.com:33964/mytasklist', ['tasks'])
 
 //GET
 router.get('/tasks', (req,res,next) => {
