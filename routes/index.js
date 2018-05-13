5055a6bf61613378fb73afd78a3cd984257af2c3
Eric Laursen <ericjlaursen@gmail.com>
2018-05-13 07:14:21 -0400

    Moves app into client folder.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 //GET
-router.get('/', (req,res,next) => {
-	res.render('index.html')
-})
+router.get('/', function(req, res, next){
+    res.render('index.html');
+});
 
 module.exports = router;
