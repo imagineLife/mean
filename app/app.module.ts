37dd47f3eaa9750d94d4985e2f5cc7bd4cc48efc
Eric Laursen <ericjlaursen@gmail.com>
2018-05-12 20:49:25 -0400

    Imports appComponent.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {AppComponent} from './app.component';
+
 @NgModule({
-	imports: [ BroswerModule ]
+	imports: [ BroswerModule ],
+	declarations: [AppComponent],
+	bootstrap: [AppComponent]
 })
 exports class AppModule { }
